add8d79c3943a35f526165737d08571c3c19a801
Joe Ferris <joe.r.ferris@gmail.com>
2011-01-13 17:26:25 -0500

    Install script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+cutstring="DO NOT EDIT BELOW THIS LINE"
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e $target ]; then
+    if [ ! -L $target ]; then
+      cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
+      if [[ -n $cutline ]]; then
+        let "cutline = $cutline - 1"
+        echo "Updating $target"
+        head -n $cutline "$target" > update_tmp
+        startline=`tail -r "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
+        if [[ -n $startline ]]; then
+          tail -n $startline "$name" >> update_tmp
+        else
+          cat "$name" >> update_tmp
+        fi
+        mv update_tmp "$target"
+      else
+        echo "WARNING: $target exists but is not a symlink."
+      fi
+    fi
+  else
+    if [[ $name != 'install.sh' ]]; then
+      echo "Creating $target"
+      if [[ -n `grep "$cutstring" "$name"` ]]; then
+        cp "$PWD/$name" "$target"
+      else
+        ln -s "$PWD/$name" "$target"
+      fi
+    fi
+  fi
+done
