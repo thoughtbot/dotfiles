e892af44d37c5df612b8be2ad1c0c14c103d6693
Josh Steiner <josh@jsteiner.me>
2013-07-26 14:27:51 -0400

    Remove unused zsh keybindings

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,16 +1,10 @@
 # vi mode
 bindkey -v
 bindkey "^F" vi-cmd-mode
-bindkey jj vi-cmd-mode
 
 # use incremental search
 bindkey "^R" history-incremental-search-backward
 
-# add some readline keys back
-bindkey "^A" beginning-of-line
-bindkey "^E" end-of-line
-
-# handy keybindings
 bindkey "^P" history-search-backward
 bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
