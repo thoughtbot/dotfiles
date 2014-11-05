3e4746e1da679aef03cab0f189ee561686ad7719
Jesse Armand <jesse@jessearmand.com>
2014-11-05 10:55:05 +0800

    Had switched to chruby from rbenv.

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,5 +1,3 @@
-export PATH="$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
 
--- ruby/rbenv.zsh
+++
@@ -1,6 +0,0 @@
-# load rbenv if available
-if which rbenv &>/dev/null ; then
-  eval "$(rbenv init - zsh --no-rehash)"
-fi
-
-
