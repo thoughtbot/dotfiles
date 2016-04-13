5b2dba1c760d3f6f415f6e878cd1cfeb940b444b
Jesse Armand <jesse.armand@gotouche.com>
2016-04-13 10:16:35 +0800

    Fixed Postgres.app path

--- postgres/path.zsh
+++ postgres/path.zsh
@@ -1,8 +1,8 @@
 # Postgres app
 
-export POSTGRES_BIN_PATH=/Applications/Postgres93.app/Contents/MacOS/bin
-export POSTGRES_INCLUDE_PATH=/Applications/Postgres93.app/Contents/MacOS/include
-export POSTGRES_LIB_PATH=/Applications/Postgres93.app/Contents/MacOS/lib
+export POSTGRES_BIN_PATH=/Applications/Postgres.app/Contents/MacOS/bin
+export POSTGRES_INCLUDE_PATH=/Applications/Postgres.app/Contents/MacOS/include
+export POSTGRES_LIB_PATH=/Applications/Postgres.app/Contents/MacOS/lib
 export POSTGRES_SHARED_LIB_PATH=/Applications/Postgres.app/Contents/MacOS/share
 export POSTGRES_DATA_PATH="$HOME/Library/Application Support/Postgres/var"
 
