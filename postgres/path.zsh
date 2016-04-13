1900bca38a3b769c38d88f2991d9eeb26043ddf6
Jesse Armand <jesse.armand@gotouche.com>
2016-04-13 10:23:38 +0800

    Updated Postgres.app path to latest

--- postgres/path.zsh
+++ postgres/path.zsh
@@ -1,9 +1,9 @@
 # Postgres app
 
-export POSTGRES_BIN_PATH=/Applications/Postgres.app/Contents/MacOS/bin
-export POSTGRES_INCLUDE_PATH=/Applications/Postgres.app/Contents/MacOS/include
-export POSTGRES_LIB_PATH=/Applications/Postgres.app/Contents/MacOS/lib
-export POSTGRES_SHARED_LIB_PATH=/Applications/Postgres.app/Contents/MacOS/share
+export POSTGRES_BIN_PATH=/Applications/Postgres.app/Contents/Versions/latest/bin
+export POSTGRES_INCLUDE_PATH=/Applications/Postgres.app/Contents/Versions/latest/include
+export POSTGRES_LIB_PATH=/Applications/Postgres.app/Contents/Versions/latest/lib
+export POSTGRES_SHARED_LIB_PATH=/Applications/Postgres.app/Contents/Versions/latest/share
 export POSTGRES_DATA_PATH="$HOME/Library/Application Support/Postgres/var"
 
 export PATH=$POSTGRES_BIN_PATH:$PATH
