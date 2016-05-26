0e5a43b51fadddb63e247190a5448a75fcde309d
Jesse Armand <jesse@jessearmand.com>
2016-05-27 02:24:25 +0800

    `which node` is not working

--- node/path.zsh
+++
@@ -1,9 +0,0 @@
-# node modules
-NODE_BINARY=`which node`
-NODE_PREFIX=${NODE_BINARY%/bin/node}
-NODE_PATH=${NODE_PREFIX}/lib/node_modules 
-
-# Setting PATH for node package manager
-PATH="${NODE_PREFIX}/bin:$PATH"
-export PATH
-
