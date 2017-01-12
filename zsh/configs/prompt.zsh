bc24ce661ad7f42bd48075e9102363557ef52bfd
Michael Anhari <mpanhari@gmail.com>
2017-01-12 10:41:09 -0500

    Add git dirty status to zsh prompt
    - Displays a red exclamation mark when unstaged changes exist
    - Displays a blue exclamation mark when staged changes exist
    - Display a cyan asterisk when unpushed commits exist

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,9 +1,28 @@
 # modify the prompt to contain git branch name if applicable
 git_prompt_info() {
-  current_branch=$(git current-branch 2> /dev/null)
-  if [[ -n $current_branch ]]; then
-    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
+  local ref=$(=git symbolic-ref HEAD 2> /dev/null)
+  local gitst="$(=git status 2> /dev/null)"
+
+  if [[ -f .git/MERGE_HEAD ]]; then
+    if [[ ${gitst} =~ "unmerged" ]]; then
+      gitstatus=" %{$fg[red]%}unmerged%{$reset_color%}"
+    else
+      gitstatus=" %{$fg[green]%}merged%{$reset_color%}"
+    fi
+  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
+    gitstatus=" %{$fg[blue]%}!%{$reset_color%}"
+  elif [[ ${gitst} =~ "use \"git add" ]]; then
+    gitstatus=" %{$fg[red]%}!%{$reset_color%}"
+  elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
+    gitstatus=" %{$fg[cyan]%}*%{$reset_color%}"
+  else
+    gitstatus=''
+  fi
+
+  if [[ -n $ref ]]; then
+    echo "%{$fg_bold[orange]%} ${ref#refs/heads/}%{$reset_color%}$gitstatus"
   fi
 }
+
 setopt promptsubst
 PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
