ad2e16e38e453b748a5f8b87dbe339a3cce0f0f8
Brett MacDonald <ArtistsTechGuy@users.noreply.github.com>
2018-05-15 01:31:02 -0300

    Improve successive startup time; fix conflict.
    - Don't update completion dump if it has been updated within 24hrs.
    - Unload Completion for zsh bundled function mcd to prevent conflict. #594

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -1,6 +1,14 @@
 # load our own completion functions
 fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 
-# completion
-autoload -U compinit
-compinit
+# completion; use cache if updated within 24h
+autoload -Uz compinit
+if [[ -n $HOME/.zcompdump(#qN.mh+24) ]]; then
+  compinit -d $HOME/.zcompdump;
+else
+  compinit -C;
+fi;
+
+# disable zsh bundled function mtools command mcd
+# which causes a conflict.
+compdef -d mcd
