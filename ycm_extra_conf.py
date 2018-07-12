84d0c28a29db6df4f2f6e641bddc3734edceebec
Jesse Armand <jesse.armand@gotouche.com>
2018-07-12 14:08:22 +0800

    Fixed python setup with vim and ycm

--- ycm_extra_conf.py
+++ ycm_extra_conf.py
@@ -45,7 +45,7 @@ flags = [
 '-DNDEBUG',
 # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
 # source code needs it.
-'-DUSE_CLANG_COMPLETER',
+# '-DUSE_CLANG_COMPLETER',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
