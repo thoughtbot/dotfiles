2af1b501249714b6197eb1b60114e222dc67c96b
Daniel Barber <hello@danbarber.me>
2019-04-23 13:13:42 -0400

    Account for asdf being installed by Homebrew
    If `asdf` has been installed via Homebrew the completions file is in a
    different place. This PR takes that possibility into account.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,9 +1,11 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# load ASDF, falling back to rbenv if not available
-if [ -d "$HOME/.asdf" ]; then
+# load ASDF, either from home dir or brew install
+if [ -f "$HOME/.asdf/asdf.sh" ]; then
   . $HOME/.asdf/asdf.sh
+elif ASDF_PREFIX=$(brew --prefix asdf); then
+  . $ASDF_PREFIX/asdf.sh
 fi
 
 # mkdir .git/safe in the root of repositories you trust
