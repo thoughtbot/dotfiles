971d5832c3182e8a9e5db56c92cc8cc795754f80
Mike Burns <mburns@thoughtbot.com>
2019-05-03 10:45:53 -0400

    Make Homebrew analytics opt-in
    Currently [Homebrew implements analytics] as opt-out, forcing it on its
    users without their enthusiastic consent or a full understanding of the
    implications.
    
    This changes it to opt-in: we opt-out by default, and to opt in to
    Homebrew analytics we can override it in `~/.zshrc.local`:
    
    ```sh
    unset HOMEBREW_NO_ANALYTICS
    ```
    
    [Homebrew implements analytics]: https://docs.brew.sh/Analytics

---
+++ zsh/configs/homebrew.zsh
@@ -0,0 +1,4 @@
+# To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
+# Learn more about what you are opting in to at
+# https://docs.brew.sh/Analytics
+export HOMEBREW_NO_ANALYTICS=1
