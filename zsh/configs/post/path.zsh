cdd0d2ced80ea9f80bbb1d6e6b15817e3c1f7840
Daniel Barber <github@danbarber.me>
2019-07-16 17:47:48 -0400

    Assume people don't change their Homebrew dir
    It checks the home directory location and the default Homebrew location
    before falling back to `brew --prefix` (Sloooow!)
    
    This also accounts for non macOS systems that don't have Homebrew.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -3,9 +3,11 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
 # load ASDF, either from home dir or brew install
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
-  . $HOME/.asdf/asdf.sh
-elif ASDF_PREFIX=$(brew --prefix asdf); then
-  . $ASDF_PREFIX/asdf.sh
+  . "$HOME/.asdf/asdf.sh"
+elif [ -f "/usr/local/opt/asdf/asdf.sh" ]; then
+  . "/usr/local/opt/asdf/asdf.sh"
+elif which brew >/dev/null && ASDF_PREFIX=$(brew --prefix asdf); then
+  . "$ASDF_PREFIX/asdf.sh"
 fi
 
 # mkdir .git/safe in the root of repositories you trust
