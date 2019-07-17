8b7e8db06395e60097b169a582998c69ca67ddb4
Daniel Barber <github@danbarber.me>
2019-07-17 10:05:54 -0400

    `brew --prefix` will return something whether ASDF is installed or not
    So we should check that the file exists before we try calling it.

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,12 +1,16 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# load ASDF, either from home dir or brew install
+# Try loading ASDF from the regular home dir location
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
   . "$HOME/.asdf/asdf.sh"
+# It's not in the home dir, let's try the default Homebrew location
 elif [ -f "/usr/local/opt/asdf/asdf.sh" ]; then
   . "/usr/local/opt/asdf/asdf.sh"
-elif which brew >/dev/null && ASDF_PREFIX=$(brew --prefix asdf); then
+# Not there either! Last try, let's see if Homebrew can tell us where it is
+elif which brew >/dev/null &&
+  ASDF_PREFIX=$(brew --prefix asdf) &&
+  [ -f "$ASDF_PREFIX/asdf.sh" ]; then
   . "$ASDF_PREFIX/asdf.sh"
 fi
 
