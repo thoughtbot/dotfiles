57e23e0e67c37f6f77d0f3a53bdeafaad8174976
Daniel Barber <github@danbarber.me>
2019-07-17 17:50:56 -0400

    Dirname is clearer here

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -5,7 +5,7 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
   . "$HOME/.asdf/asdf.sh"
 elif which brew >/dev/null &&
-  BREW_DIR=$(which brew | sed 's/\/bin\/brew//') &&
+  BREW_DIR="$(dirname `which brew`)/.." &&
   [ -f "$BREW_DIR/opt/asdf/asdf.sh" ]; then
   . "$BREW_DIR/opt/asdf/asdf.sh"
 fi
