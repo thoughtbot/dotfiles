5001da339e86d04de0008f68e0492a168e0004e8
Kamil Nicieja <kamil@nicieja.co>
2022-03-31 02:25:52 +0200

    Configure puma

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ bash ./macos/install.sh
 bash ./vscode/install.sh
 touch ~/Library/Application\ Support/Code/User/settings.json
 cp -f ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+bash ./puma/install.sh
