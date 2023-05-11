dfafe01fefa784dbdf780f13e4de984edbdad330
Thomas Ricard <thomas.ricard@gmail.com>
2023-05-11 16:58:05 -0700

    e

--- .github/slack-pr-notification.rb
+++ .github/slack-pr-notification.rb
@@ -5,31 +5,50 @@
 require 'slack-ruby-client'
 require 'json'
 
-# load json object from file
-json = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
-puts json
-
+# configure slack with the right
 Slack.configure do |config|
   config.token = ENV['SLACK_GITHUB_BOT_TOKEN']
 end
-
-pr_author_email = ENV['pull_request_author_email']
-
 client = Slack::Web::Client.new()
-resp = client.users_lookupByEmail(email: pr_author_email)
+slack_user = find_slack_user_from_email(ENV['pull_request_author_email'])
+if !slack_user
+  puts "No slack user found for #{ENV['pull_request_author_email']}"
+  exit 0
+end
 
-if resp.ok?
-  user = resp.user
+# load json object from file
+event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+puts event
+
+action = event['action']
+if action == 'edited' and action['changes']['title']
+  old_title = action['changes']['title'][]
+
+  # find the conversation with the bot
   resp = client.users_conversations(types: 'im')
   channel = resp.channels.select { |c| c.user == user.id }.first
-  messages = client.conversations_history(channel: channel.id).messages
-
-  message = messages.select { |m| m.text == 'Hello World2' }.first
-  puts message
-
-  if message
-    client.chat_postMessage(channel: user.id, text: 'An update', as_user: true, thread_ts: message.ts)
+  if channel != nil
+    # try to find if there is already an existing message
+    messages = client.conversations_history(channel: channel.id).messages
+    message = messages.select { |m| m.text == old_title }.first
+    if message
+      client.chat_postMessage(channel: user.id, text: 'An update', as_user: true, thread_ts: message.ts)
+    else
+      client.chat_postMessage(channel: user.id, text: event['pull_request']['title'], as_user: true)
+    end
   else
-    client.chat_postMessage(channel: user.id, text: 'Hello World2', as_user: true)
+    client.chat_postMessage(channel: user.id, text: event['pull_request']['title'], as_user: true)
   end
+end
+
+def find_slack_user_from_email(email)
+  begin
+    resp = client.users_lookupByEmail(email: email)
+    if resp.ok?
+      resp.user
+    end
+  rescue => e
+    puts e
+  end
+  nil
 end
