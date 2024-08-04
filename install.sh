237691f37c52f18ea80069e067c728ed32a09981
Kamil Nicieja <kamil@nicieja.co>
2024-08-04 00:32:27 -0700

    Update Zed script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 bash ./macos/install.sh
 bash ./asdf/install.sh
 bash ./puma/install.sh
+bash ./zed/install.sh
 
 ln -s $PWD ~/.dotfiles
 env RCRC=$PWD/rcrc rcup
---
+++ zed/install.sh
@@ -0,0 +1,2 @@
+rm -rf ~/.config/zed/settings.json
+ln -s settings.json ~/.config/zed/settings.json
