51c2dd868b9ab26a55b5f41fa05b188ea1d6342e
Kamil Nicieja <kamil@nicieja.co>
2024-09-28 00:54:09 -0700

    Update prompts

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,36 +1,11 @@
 asdf plugin add nodejs
 asdf plugin add ruby
-asdf plugin add postgres
 asdf plugin add elixir
-asdf plugin add rabbitmq
-asdf plugin add redis
-asdf plugin add mongodb
 
 asdf install nodejs latest
 asdf install ruby latest
-asdf install postgres latest
 asdf install elixir latest
-asdf install rabbitmq latest
-asdf install redis latest
-asdf install mongodb latest
 
 asdf global nodejs $(asdf latest nodejs)
 asdf global ruby $(asdf latest ruby)
-asdf global postgres $(asdf latest postgres)
 asdf global elixir $(asdf latest elixir)
-asdf global rabbitmq $(asdf latest rabbitmq)
-asdf global redis $(asdf latest redis)
-asdf global mongodb $(asdf latest mongodb)
-
-sudo cp ./asdf/org.postgresql.postgres.plist /Library/LaunchDaemons/
-sudo cp ./asdf/org.redis.redis.plist /Library/LaunchDaemons/
-
-sudo chown root:wheel /Library/LaunchDaemons/org.redis.redis.plist
-sudo chmod 644 /Library/LaunchDaemons/org.redis.redis.plist
-
-sudo chown root:wheel /Library/LaunchDaemons/org.postgresql.postgres.plist
-sudo chmod 644 /Library/LaunchDaemons/org.postgresql.postgres.plist
-
-sudo launchctl load /Library/LaunchDaemons/org.postgresql.postgres.plist
-sudo launchctl load /Library/LaunchDaemons/org.redis.redis.plist
-
---
+++ zed/prompts/architecture.md
@@ -0,0 +1,9 @@
+You are an experienced software architect collaborating with me on a project. I am a senior software engineer specializing in Ruby on Rails. Your role is to:
+
+* Provide architectural guidance and strategic insights.
+* Suggest best practices specific to Ruby on Rails applications.
+* Help design scalable, maintainable, and efficient software solutions.
+* Discuss potential design patterns and architectural trade-offs.
+* Assist in making informed decisions to enhance the software design.
+
+Please communicate in a collaborative and professional manner, asking questions when necessary to better understand project requirements. Let's work together to build robust and effective applications.
