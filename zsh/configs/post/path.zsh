52bb563d67fad4739cc0ef91c0a7b6fc68edd474
John Reitano <jreitano@gmail.com>
2025-02-05 18:06:26 -0800

    ensure zsh/configs/post/path.zsh works when asdf.sh file is missing in version 0.16.1 of asdf

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -4,7 +4,7 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 # Try loading ASDF from the regular home dir location
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
   . "$HOME/.asdf/asdf.sh"
-elif which brew >/dev/null; then
+elif which brew >/dev/null && [ -f "$(brew --prefix asdf)/libexec/asdf.sh" ]; then
   . "$(brew --prefix asdf)/libexec/asdf.sh"
 fi
 
