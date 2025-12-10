d8defee03a4fd2b5c704f776cf44846d29598677
Rakesh Arunachalam <rakesh@thoughtbot.com>
2025-12-10 15:19:23 +0000

    improve zsh history

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -1,6 +1,9 @@
-setopt hist_ignore_all_dups inc_append_history
+#!/usr/bin/env zsh
+
+setopt hist_ignore_all_dups hist_ignore_space inc_append_history share_history
+
 HISTFILE=~/.zhistory
-HISTSIZE=4096
-SAVEHIST=4096
+HISTSIZE=8192
+SAVEHIST=8192
 
 export ERL_AFLAGS="-kernel shell_history enabled"
